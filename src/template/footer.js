1159dcc6f8a8e693aed4645cee5c97812870c766
Daniil <danilka1337d1@gmail.com>
2024-03-08 22:44:58 +0200

    added focus section in home page

--- src/template/footer.js
+++ src/template/footer.js
@@ -7,52 +7,54 @@ import github from '../assets/img/footer/socials/github.svg'
 
 const footerHtml = `
 <footer>
-<div class="footer footer__container-big">
-    <div class="footer-socials">
-        <a class="footer-socials__logo"><img src="${logo}" alt="logo"></a>
-        <div class="footer-socials__items">
-            <a href="" class="footer-socials__item"><img src="${facebook}"
+<div class="footer">
+    <div class="footer__container-big">
+        <div class="footer-socials">
+            <a href="" class="footer-socials__logo"><img src="${logo}" alt="logo"></a>
+            <div class="footer-socials__items">
+                <a href="" class="footer-socials__item"><img src="${facebook}"
                     alt="facebook"></a>
-            <a href="" class="footer-socials__item"><img src="${linkedin}"
+                <a href="" class="footer-socials__item"><img src="${linkedin}"
                     alt="logo"></a>
-            <a href="" class="footer-socials__item"><img src="${telephone}"
+                <a href="" class="footer-socials__item"><img src="${telephone}"
                     alt="logo"></a>
-            <a href="" class="footer-socials__item"><img src="${skype}"
+                <a href="" class="footer-socials__item"><img src="${skype}"
                     alt="logo"></a>
-            <a href="" class="footer-socials__item"><img src="${github}"
+                <a href="" class="footer-socials__item"><img src="${github}"
                     alt="logo"></a>
+            </div>
         </div>
-    </div>
-    <div class="footer-services">
-        <a href="" class="footer-services__text">Services</a>
-        <div class="footer-services__items">
-            <a href='' class="footer-services__item">Mobile Application Development</a>
-            <a href='' class="footer-services__item">Game Development Outsourcing</a>
-            <a href='' class="footer-services__item">VR App Development</a>
-            <a href='' class="footer-services__item">Dedicated Developers</a>
-            <a href='' class="footer-services__item">SDK Development</a>
-            <a href='' class="footer-services__item">3D Modeling services</a>
+        <div class="footer-services">
+            <a href="" class="footer-services__text">Services</a>
+            <div class="footer-services__items">
+                <a href='' class="footer-services__item">Mobile Application Development</a>
+                <a href='' class="footer-services__item">Game Development Outsourcing</a>
+                <a href='' class="footer-services__item">VR App Development</a>
+                <a href='' class="footer-services__item">Dedicated Developers</a>
+                <a href='' class="footer-services__item">SDK Development</a>
+                <a href='' class="footer-services__item">3D Modeling services</a>
+            </div>
         </div>
-    </div>
-    <div class="footer-technologies">
-        <a href="" class="footer-technologies__text">Technologies </a>
-        <div class="footer-technologies__items">
-            <a href='' class="footer-technologies__item">Android</a>
-            <a href='' class="footer-technologies__item">IOS</a>
-            <a href='' class="footer-technologies__item">Unity</a>
+        <div class="footer-technologies">
+            <a href="" class="footer-technologies__text">Technologies </a>
+            <div class="footer-technologies__items">
+                <a href='' class="footer-technologies__item">Android</a>
+                <a href='' class="footer-technologies__item">IOS</a>
+                <a href='' class="footer-technologies__item">Unity</a>
+            </div>
         </div>
-    </div>
-    <div class="footer-products">
-        <a href="" class="footer-products__text">products </a>
-        <a href="" class="footer-products__text">Portfolio </a>
-        <a href="" class="footer-products__text">Blog </a>
-    </div>
-    <div class="footer-about">
-        <a href="" class="footer-about__text">About us </a>
-        <div class="footer-about__items">
-            <a href='' class="footer-about__item">Contacts</a>
-            <a href='' class="footer-about__item">Team</a>
-            <a href='' class="footer-about__item">Join the Team</a>
+        <div class="footer-products">
+            <a href="" class="footer-products__text">products </a>
+            <a href="" class="footer-products__text">Portfolio </a>
+            <a href="" class="footer-products__text">Blog </a>
+        </div>
+        <div class="footer-about">
+            <a href="" class="footer-about__text">About us </a>
+            <div class="footer-about__items">
+                <a href='' class="footer-about__item">Contacts</a>
+                <a href='' class="footer-about__item">Team</a>
+                <a href='' class="footer-about__item">Join the Team</a>
+            </div>
         </div>
     </div>
 </div>
